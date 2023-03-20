d413202e6e24523f3c37c12e56a44fe255cefd8e
Olger Männik <Olgerm@protonmail.com>
2023-03-20 22:32:47 +0200

    URLs to tables do not end with ".html" anymore.

--- src/main/java/com/cgi/library/controller/FrontendController.java
+++ src/main/java/com/cgi/library/controller/FrontendController.java
@@ -9,10 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("")
 public class FrontendController {
-
     @GetMapping("")
     public String MainPage() {
         return "main_page.html";
     }
 
+    @GetMapping("table_of_books")
+    public String TableOfBooks() {
+        return "table_of_books.html";
+    }
+
+    @GetMapping("/table_of_checkouts")
+    public String TableOfCheckouts() {
+        return "table_of_checkouts.html";
+    }
 }
